57b402c6c2b2ea0f0da244f9143eae7fcde51745
thakurkaran007 <thakurkaran0345@example.com>
2026-01-29 12:52:12 +0530

    ci-cd/integration

--- integration-test/scripts/run-integrations-test.sh
+++ integration-test/scripts/run-integrations-test.sh
@@ -4,6 +4,8 @@ echo '🟡 - Waiting for database to be ready...'
 
 scripts/wait-for-it.sh "postgresql://karan:thakur@localhost:5432/db_karan" -- echo '🟢 - Database is ready!'
 
+npm install
+
 npx prisma migrate dev --name init
 
 npm run test
